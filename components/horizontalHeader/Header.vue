28b6e72564b4c46dff587bd7ad6fdf14dfd36f2f
peppineddu5 <59139796+peppineddu5@users.noreply.github.com>
2022-06-16 12:05:38 +0200

    [ADD] I add Header on component

---
+++ components/horizontalHeader/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="flex flex-col justify-start	">
+        <horizontalHeaderButton to="/home" name="Profile" notification="2" />
+        <horizontalHeaderButton to="/hodme" name="Favourite" notification="2" />
+        <horizontalHeaderButton to="/hodme" name="dsa" notification="2" />
+    </header>
+</template>
+
+<style>
+header {
+    width: 242px;
+    background-color: var(--light4);
+    height: 100vh;
+    border-right: 1px solid #f2f2f2;
+}
+</style>
