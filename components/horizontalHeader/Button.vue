7400910745c920707a4962756f230127206a5e45
peppineddu5 <59139796+peppineddu5@users.noreply.github.com>
2022-06-16 12:07:56 +0200

    [ADD] button component

---
+++ components/horizontalHeader/Button.vue
@@ -0,0 +1,77 @@
+<template>
+    <NuxtLink :to="to" style="width:calc(100% - 10px);">
+        <div class="flex flex-row items-center buttonHeader">
+            <div class="icon"></div>
+            <h1 class="text">{{ name }}</h1>
+            <div v-if="notification" class="notification ">
+                <h4>{{ notification }}</h4>
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<style>
+.buttonHeader {
+    height: auto;
+    width: 100%;
+    padding: 8px;
+    border-top-right-radius: 30px;
+    border-bottom-right-radius: 30px;
+    
+}
+.buttonHeader:hover {
+    background-color: #64646434;
+    
+
+}
+
+.buttonHeader .icon {
+    width: 24px;
+    height: 24px;
+    border-radius: 100%;
+    background-color: var(--light6);
+    margin-left: 25px;
+    margin-right:20px ;
+}
+
+.buttonHeader h1 {
+    margin: 0 25px;
+}
+
+.buttonHeader .text {
+    width: 23%;
+    color: var(--light6);
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 19px;
+}
+
+.buttonHeader .notification {
+
+    width: 18px;
+    height: 18px;
+    padding: 1px;
+    border-radius: 100%;
+    background-color: var(--light1);
+   display: flex; /* or inline-flex */
+  align-items: center; 
+  justify-content: center;
+}
+
+.buttonHeader .notification h4 {
+    font-weight: 600;
+    font-size: 10px;
+    line-height: 13px;
+    color: var(--light4);
+}
+</style>
+<script>
+export default {
+    props: {
+        to: String,
+        name: String,
+        notification: Number,
+        icon: String
+    }
+}
+</script>
