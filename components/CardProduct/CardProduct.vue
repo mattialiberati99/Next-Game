ee2acda4cef9e76f34a955d14a3778e49b33f470
Justy116 <giustinogragnaniello116@gmail.com>
2022-06-28 14:56:00 +0200

    [ADD] tailwind style

--- components/CardProduct/CardProduct.vue
+++ components/CardProduct/CardProduct.vue
@@ -1,42 +1,29 @@
 <template>
-    <!-- <div class="card">
-        <img :src="item.pathImgCard" class="imgCardProduct" :alt="item.nameProduct" />
-        <div class="container-product">
-            <h3 class="titleCard"><b>{{item.nameProduct}}</b></h3>
-            <div class="container-categoriesCard">
-                <p class="categoriesCard">{{item.categories1}}</p>
-                <span class="dot"></span> 
-                <p class="categoriesCard">{{item.categories2}}</p>
-            </div>
-            <span class="valutationCard">{{item.valutation}}</span> 
-            <span class="starCard">⭐</span>
-            <span class="priceCard"><b>{{item.price}}</b></span>
-            <div class="container-logos-img-Carduser">
-                <div class="system-operator-logos">
-                   <img class="img-windows" src="../../assets/icons/windows.svg" alt="logo windows">
-                   <img class="img-apple" src="../../assets/icons/apple.svg" alt="logo apple">
-                </div>
-                <div class="container-img-cardUser">
-                    <img :src="item.imgUser" alt="">
-                    <img :src="item.imgUser" alt="">
-                    <img :src="item.imgUser" alt="">
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <div class="rounded-3xl h-75 w-64 p-3 bg-gray-50">
+    <div class="rounded-3xl h-75 w-64 p-3 bg-gray-50 mt-5">
         <img class="w-full rounded-3xl w-60 h-48 object-cover" :src="item.pathImgCard" alt="Sunset in the mountains" />
         <div class="px-6 py-4">
-          <h2 class="font-bold text-l mb-2">{{item.nameProduct}}</h2>
-          <div class="flex text-sm">
+          <h2 class="font-bold text-l">{{item.nameProduct}}</h2>
+          <div class="flex text-sm container-categories items-center">
             <p>{{item.categories1}}</p>
-            <span class="dot"></span>
+            <span class="dot block rounded-full w-1 h-1"></span>
             <p>{{item.categories2}}</p>
           </div>
-          <div class="flex text-sm">
+          <div class="flex text-sm container-price items-center mt-1 mb-1">
             <p>{{item.valutation}}</p>
-            <p>⭐</p>
-            <h3 class="price">{{item.price}}</h3>
+            <svg class="star h-4 mr-2.5 ml-2.5" xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="none"><path d="M7.94838 0.778719C7.86415 0.60805 7.69034 0.5 7.50001 0.5C7.30969 0.5 7.13587 0.60805 7.05165 0.778719L5.00476 4.92621L0.42816 5.59081C0.239806 5.61816 0.0833166 5.75008 0.0244934 5.93109C-0.0343298 6.1121 0.0147161 6.31081 0.151008 6.44366L3.46277 9.67185L2.68076 14.2306C2.64858 14.4182 2.72569 14.6078 2.87966 14.7196C3.03364 14.8315 3.23778 14.8463 3.40624 14.7577L7.50001 12.6054L11.5938 14.7577C11.7623 14.8463 11.9664 14.8315 12.1204 14.7196C12.2743 14.6078 12.3515 14.4182 12.3193 14.2306L11.5373 9.67185L14.849 6.44366C14.9853 6.31081 15.0344 6.1121 14.9755 5.93109C14.9167 5.75008 14.7602 5.61816 14.5719 5.59081L9.99527 4.92621L7.94838 0.778719Z" fill="orange"/></svg>
+            <h3 class="price font-bold">{{item.price}}</h3>
+            <p class="symbol-dollar font-bold" v-if="item.price !== 'Free'"> USD</p>
+          </div>
+          <div class="flex justify-between">
+            <div class="flex">
+                   <img class="img-windows h-4" src="../../assets/icons/windows.svg" alt="logo windows">
+                   <img class="img-apple h-4" src="../../assets/icons/apple.svg" alt="logo apple">
+            </div>
+            <div class="flex">
+                <img class="rounded-full w-6 border-white border-2 -mr-1" :src="item.imgUser" alt="user image">
+                <img class="rounded-full w-6 border-white border-2 -mr-1" :src="item.imgUser" alt="user image">
+                <img class="rounded-full w-6 border-white border-2 -mr-1" :src="item.imgUser" alt="user image">
+            </div>
           </div>
         </div>
     </div>
@@ -52,101 +39,18 @@ export default {
 </script>
 
 <style>
-/* 
-.card {
-    background-color: rgb(255, 255, 255);
-    max-width: 258px;
-    padding: 10px;
-    border-radius: 25px;
-    margin-top: 20px;
-}
 
-.imgCardProduct {
-    height: 195.25px;
-    width: 238px;
-    border-radius: 25px;
-    margin-bottom: 20px;
-    object-fit: cover;
-}
-
-.container-product {
-    padding-left: 10px;
-}
-
-.titleCard {
-    font-size: 16px;
-}
-
-.container-categoriesCard {
-    display: flex;
-    align-items: center;
-}
-
-.valutationCard,
-.categoriesCard{
-    color: #767676;
-    font-size: 14px;
-    line-height: 19px;
-}
-
-.dot{
-    border-radius: 50%;
-    display: block;
-    background-color: #767676;
-    width: 3px;
-    height: 2.96px;
-    margin: 0 10px 0 10px;
-}
-
-.priceCard {
-    font-size: 14px;
-    color: #616161;
-}
-
-.system-operator-logos {
-    display: flex;
-}
-
-.img-windows,
-.img-apple {
-    height: 20px;
-    margin-right: 10px;
-}
-
-.container-logos-img-Carduser {
-    margin: 10px 0;
-    display: flex;
-    justify-content: space-between;
-}
-
-.container-img-cardUser {
-    display: flex;
-    height: 24px;
-}
-
-.container-img-cardUser > img {
-    border: 2px solid white;
-    border-radius: 50%;
-    margin-right: -5px;
-} */
-
-.flex {
-    align-items: center;
+.container-categories,
+.container-price,
+.dot {
     color: #767676;
 }
 
-.price {
+.price,
+.symbol-dollar {
     color: #616161;
 }
 
-.dot{
-    border-radius: 50%;
-    display: block;
-    background-color: #767676;
-    width: 3px;
-    height: 2.96px;
-    margin: 0 10px 0 10px;
-}
 
 </style> 
 
--- components/ContainerCard/ContainerCard.vue
+++ components/ContainerCard/ContainerCard.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container-card">
+  <div class="flex flex-wrap">
       <CardProduct v-for="card in cards" v-bind:item="card" :key="card.id"/>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -19,86 +20,86 @@ export default {
                     price: "Free",
                     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
                 },
-                // {
-                //     id: 2,
-                //     pathImgCard: "https://cdn.akamai.steamstatic.com/steam/apps/975270/header.jpg?t=1626291858",
-                //     nameProduct: "Legion War",
-                //     categories1: "Strategy",
-                //     categories2: "Adventure",
-                //     valutation: 4.1,
-                //     price: "$9.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 3,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 4,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 5,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 6,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 7,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 8,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // },
-                // {
-                //     id: 9,
-                //     pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
-                //     nameProduct: "Warpips",
-                //     categories1: "Strategy",
-                //     categories2: "Tower Defence",
-                //     valutation: 4.5,
-                //     price: "$11.99 USD",
-                //     imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
-                // }
+                {
+                    id: 2,
+                    pathImgCard: "https://cdn.akamai.steamstatic.com/steam/apps/975270/header.jpg?t=1626291858",
+                    nameProduct: "Legion War",
+                    categories1: "Strategy",
+                    categories2: "Adventure",
+                    valutation: 4.1,
+                    price: "$9.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 3,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 4,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 5,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 6,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 7,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 8,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                },
+                {
+                    id: 9,
+                    pathImgCard: "https://steamcdn-a.akamaihd.net/steam/apps/1291010/header.jpg",
+                    nameProduct: "Warpips",
+                    categories1: "Strategy",
+                    categories2: "Tower Defence",
+                    valutation: 4.5,
+                    price: "$11.99",
+                    imgUser: "https://www.w3schools.com/w3css/img_avatar3.png"
+                }
             ]
         }
     }
@@ -106,11 +107,5 @@ export default {
 </script>
             
 <style>
-
-.container-card {     
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-}
 
 </style>
