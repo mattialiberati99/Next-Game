df82f673cfe88dd8f0d1a2740fed68e8c78a55f1
Justy116 <giustinogragnaniello116@gmail.com>
2022-07-01 15:54:07 +0200

    [UPD] update loginForm add new css

--- components/loginPage/LoginPage.vue
+++ components/loginPage/LoginPage.vue
@@ -4,21 +4,21 @@
         <img class="h-12" src="../../assets/icons/Logo.png" alt="logo">
     </div>
     <div class="form bg-white rounded-3xl p-10 mb-10 mt-20">
-      <div class="flex justify-center pt-3">
-        <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
-        <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
-        <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
+      <div class="flex justify-center pt-2">
+        <img class="h-14" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
+        <img class="h-14 ml-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
+        <img class="h-14 ml-6" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
       </div>
-      <div class="text-center mt-2">
-        <p>Or with Email</p>
+      <div class="text-center mt-6">
+        <p class="enterWithEmail">Or with Email</p>
         <form>
-          <input class="block rounded-2xl h-16 w-full mt-6 p-2 mb-6 focus:outline-none" type="text" placeholder="Username">
-          <input class="block rounded-2xl h-16 w-full mb-6 p-2 focus:outline-none" type="password" placeholder="Password">
+          <input class="block rounded-2xl h-16 w-full mt-6 p-4 mb-6 focus:outline-none" type="text" placeholder="Username">
+          <input class="block rounded-2xl h-16 w-full mb-6 p-6 focus:outline-none" type="password" placeholder="Password">
           <button class="buttonForm w-full rounded-2xl h-16">Log In</button>
         </form>
         <p class="passwordForgotten mt-6 mb-8">Forgotten password?</p>
         <hr>
-        <div class="flex justify-center mt-16">
+        <div class="flex justify-center mt-10">
           <p class="newAccount mr-2">Don't have an account</p>
           <p class="register"><a href="#">Register?</a></p>
         </div>
@@ -63,7 +63,8 @@ form input {
   color: #F46119;
 }
 
-.newAccount {
+.newAccount,
+.enterWithEmail {
   color: #6C6C6C;
 }
 
