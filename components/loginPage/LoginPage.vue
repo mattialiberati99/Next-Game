46cfec6de4cf42b74edc5f53c7e72c8a46893d2d
Justy116 <giustinogragnaniello116@gmail.com>
2022-07-01 15:07:15 +0200

    [UPD] update form

--- components/loginPage/LoginPage.vue
+++ components/loginPage/LoginPage.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="container-LoginPage h-screen flex justify-evenly pt-20">
+  <div class="container-LoginPage h-1000 flex justify-evenly">
     <div class="flex items-center">
         <img class="h-12" src="../../assets/icons/Logo.png" alt="logo">
     </div>
-    <div class="form bg-white rounded-3xl p-10">
+    <div class="form bg-white rounded-3xl p-10 mb-10 mt-20">
       <div class="flex justify-center pt-3">
         <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
         <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
         <img class="h-8 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png" alt="logo google">
       </div>
-      <div>
-        <p>or with Email</p> 
-        <form class="flex">
-          <input class="" type="text">
-          <input type="password">
-          <button></button>
+      <div class="text-center mt-2">
+        <p>Or with Email</p>
+        <form>
+          <input class="block rounded-2xl h-16 w-full mt-6 p-2 mb-6 focus:outline-none" type="text" placeholder="Username">
+          <input class="block rounded-2xl h-16 w-full mb-6 p-2 focus:outline-none" type="password" placeholder="Password">
+          <button class="buttonForm w-full rounded-2xl h-16">Log In</button>
         </form>
-        <p>Forgotten password?</p>
+        <p class="passwordForgotten mt-6 mb-8">Forgotten password?</p>
         <hr>
-        <p>Don't have an account? Register</p>
+        <div class="flex justify-center mt-16">
+          <p class="newAccount mr-2">Don't have an account</p>
+          <p class="register"><a href="#">Register?</a></p>
+        </div>
       </div>
     </div>
   </div>
@@ -28,7 +31,7 @@
 export default {
     data(){
         return{
-
+          
         }
     }
 }
@@ -44,4 +47,24 @@ export default {
   width: 540px;
   height: 634.9px;
 }
+
+form input {
+  background-color : #f2f2f2;
+  color: #6E6E6E;
+}
+
+.buttonForm {
+  background-color:#F46119;
+  color: white;
+}
+
+.passwordForgotten,
+.register {
+  color: #F46119;
+}
+
+.newAccount {
+  color: #6C6C6C;
+}
+
 </style>
